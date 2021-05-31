d482dbf126d0db0a25780e3db18116c4007edac6
James Pemantell <pbin2au@gmail.com>
2021-05-31 16:47:44 -0400

    Proof of concept for renaming a file

--- screenshot_renamer.py
+++ screenshot_renamer.py
@@ -1,21 +1,42 @@
 # from time import strftime, strptime
 import time
+import os
 
 # define variables
 picsdir = '/Users/james/Pictures'
+
+helptext = "\n 'h' - help."
+helptext += "\n 's' - skip"
+helptext += "\n 'c' - cancel rename"
+helptext += "\n 'f' - confirm rename"
+helptext += "\n 'a' - change file date"
+helptext += "\n 'g' - change incident date"
+helptext += "\n 'r' - reinitialize"
+helptext += "\n 't' = change ticket number"
+helptext += "\n 'd' = change defaults"
+helptext += "\n 'q' = quit"
 
 # define functions
 def convert_time(base_file_path, file_name):
     my_time_stamp = strftime("%Y%m%d", strptime(time.ctime(os.path.getmtime(base_file_path + '/' + file_name))))
     return my_time_stamp
 
+def new_namer(base_file_path,before_file_name,ss_date,ticket_num,ss_desc):
+    if ticket_num:
+       my_ticket_num = ticket_num + '_'
+    my_before_file = base_file_path + '/' + before_file_name
+    file_ext = before_file_name[-4:]
+    my_after_file = base_file_path + '/' + my_ticket_num  + ss_date + '_' +  ss_desc + file_ext
+    return my_after_file
+
+def renamer(before_file_name, after_file_name):
+    my_before_file = base_file_path + '/' + before_file_name
+    os.rename(before_file_name,after_file_name)
+
 # get user input
 my_input = input('Enter a date in the format YYYYMMDD: ')
 
-for my_file in os.listdir(picsdir):
-    if my_file[-4:] == '.png':
-        current_stamp = convert_time(picsdir, my_file)
-        if current_stamp == my_input:
-            print('{} {}'.format(my_file, time.ctime(os.path.getmtime('{}/{}'.format(picsdir, my_file)))))
+# make a list of png files in the base directory
+my_png_files += [my_file for my_file in os.listdir(picsdir) if my_file[-4:] == '.png']
 
     
