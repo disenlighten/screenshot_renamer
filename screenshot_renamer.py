695e3579eddab2b82a0f29f3dee551ba20a0f9d6
James Pemantell <pbin2au@gmail.com>
2021-07-30 22:10:25 -0400

    Bug fixes for feature to rename one file

--- screenshot_renamer.py
+++ screenshot_renamer.py
@@ -23,7 +23,7 @@ all_ss = os.listdir(picsdir)
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", dest="file_to_modify", help="A picture that you want to rename")
 parser.add_argument("-d", dest="ss_date", default=default_date, help="Date in the format YYYYMMDD")
-parser.add_argument("-t", dest="my_ticket_num", help="Ticket number")
+parser.add_argument("-t", dest="ticket_num", help="Ticket number")
 
 args = parser.parse_args()
 
@@ -40,15 +40,17 @@ helptext += "\n 'q' - quit"
 
 # define functions
 def convert_time(base_file_path, file_name):
-    my_time_stamp = strftime("%Y%m%d", strptime(time.ctime(os.path.getmtime(base_file_path + '/' + file_name))))
+    my_time_stamp = strftime("%Y%m%d", strptime(time.ctime(os.path.getmtime('{}/{}'.format(base_file_path,file_name)))))
     return my_time_stamp
 
 def new_namer(base_file_path,before_file_name,ss_date,ticket_num,ss_desc):
-    if ticket_num:
-       my_ticket_num = ticket_num + '_'
-    my_before_file = base_file_path + '/' + before_file_name
+    if ticket_num != "":
+       my_ticket_num = '{}_'.format(ticket_num)
+    else:
+        my_ticket_num = ""
+    my_before_file = '{}/{}'.format(base_file_path,before_file_name)
     file_ext = before_file_name[-4:]
-    my_after_file = base_file_path + '/' + my_ticket_num  + ss_date + '_' +  ss_desc + file_ext
+    my_after_file = '{}/{}{}_{}{}'.format(base_file_path,my_ticket_num,ss_date,ss_desc,file_ext)
     return my_after_file
 
 def open_file(base_file_path,current_file):
@@ -65,7 +67,7 @@ def file_navigator(user_choice,current_file,files_to_rename):
     print(new_current_file) 
     return new_current_file
 
-def rename_ui(files_to_rename,base_file_path,my_ticket_num,ss_date):
+def rename_ui(files_to_rename,base_file_path,ticket_num,ss_date):
     for current_file in files_to_rename:
         print("Current file is: {}".format(current_file))
         open_file(picsdir,current_file)
@@ -77,7 +79,7 @@ def rename_ui(files_to_rename,base_file_path,my_ticket_num,ss_date):
         elif 'd' in user_choice:
             ss_desc = input('Enter a description for the screenshot: ')
             file_ext = current_file[-4:]
-            my_after_file = new_namer(base_file_path,current_file,ss_date,my_ticket_num,ss_desc)
+            my_after_file = new_namer(base_file_path,current_file,ss_date,ticket_num,ss_desc)
             print("Rename to {}?".format(my_after_file))
             print("Enter f to confirm or c to cancel")
             user_choice = get_user_input()
@@ -89,7 +91,7 @@ def rename_ui(files_to_rename,base_file_path,my_ticket_num,ss_date):
                 current_file = file_navigator(user_choice,current_file,today_files)
                 continue
 
-def rename_ui_onefile(base_file_path,file_to_modify,my_ticket_num,ss_date):
+def rename_ui_onefile(base_file_path,file_to_modify,ticket_num,ss_date):
     print("Current file is: {}".format(file_to_modify))
     open_file(base_file_path,file_to_modify)
     # webbrowser.open('file://' + (os.path.join(picsdir,current_file)))
@@ -100,7 +102,7 @@ def rename_ui_onefile(base_file_path,file_to_modify,my_ticket_num,ss_date):
     elif 'd' in user_choice:
        ss_desc = input('Enter a description for the screenshot: ')
        file_ext = file_to_modify[-4:]
-       my_after_file = new_namer(base_file_path,file_to_modify,ss_date,my_ticket_num,ss_desc)
+       my_after_file = new_namer(base_file_path,file_to_modify,ss_date,ticket_num,ss_desc)
        print("Rename to {}?".format(my_after_file))
        print("Enter f to confirm or c to cancel")
        user_choice = get_user_input()
@@ -116,8 +118,8 @@ def get_user_input():
 # get initial input
 def initialize_values():
     ss_date  = input('Enter a date in the format YYYYMMDD: ')
-    my_ticket_num = input('Enter a JIRA ticket number: ')
-    return ss_date, my_ticket_num
+    ticket_num = input('Enter a JIRA ticket number: ')
+    return ss_date, ticket_num
 
 # main body  
 
@@ -126,19 +128,19 @@ if args.file_to_modify is not None:
     file_to_modify = args.file_to_modify
     dir_path = os.path.dirname(os.path.realpath(file_to_modify))
     ss_date = args.ss_date
-    if args.my_ticket_num is not None:
-        my_ticket_num = args.my_ticket_num
+    if args.ticket_num is not None:
+        ticket_num = args.ticket_num
     else:
-        my_ticket_num = ""
-    rename_ui_onefile(dir_path,file_to_modify,ss_date,my_ticket_num)
+        ticket_num = ""
+    rename_ui_onefile(dir_path,file_to_modify,ticket_num,ss_date)
 else:
     user_values = initialize_values()
     ss_date = user_values[0]
-    my_ticket_num = user_values[1]
+    ticket_num = user_values[1]
    
     for my_file in all_ss:
         current_stamp = convert_time(picsdir, my_file)
         if current_stamp == ss_date and my_file[-4:] == '.png':
             today_files.append(my_file)
 
-    rename_ui(today_files,picsdir,my_ticket_num,ss_date)
+    rename_ui(today_files,picsdir,ticket_num,ss_date)
